2110d0750f649b443733311b809062ed38109f26
Md Kawar <60819454+mdkawsarjomadder@users.noreply.github.com>
2024-11-20 15:34:10 +0600

    sterps

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from '../Components/Header'
+import Steps from '../Components/Steps/Steps'
+import Description from '../Components/Description/Description'
 
 const Home = () => {
   return (
-    <div></div>
+    <div>
+      <Header />
+      <Steps />
+      <Description />
+    </div>
   )
 }
 
